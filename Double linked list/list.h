60d813a5383af36486f0f2287568864282b833b6
t o <finalfantasytimothy@gmail.com>
2019-02-20 20:56:27 -0700

    Added basic stubs, btw can one of you guys consider helping with the iterator class for this assignment? It's a bit of a toughie

--- Double linked list/list.h
+++ Double linked list/list.h
@@ -1,13 +1,53 @@
 #pragma once
+#include "node.h"
+
 namespace custom
 {
+	template <class T>
 	class list
 	{
 
 	private:
+		Node <T> *pHead;
+		Node <T> *pTail;
+		int numElements;
+
 
 	public:
+		list()
+		{
+			pHead = nullptr;
+			pTail = nullptr;
+			numElements = 0;
+		}
 
+		list(list rhs)
+		{
+
+		}
+
+		list operator=(list rhs)
+		{
+
+		}
+
+		int size() { return numElements; }
+
+		bool empty() { return pHead == nullptr; }
+
+		void clear();
+		void push_back(T item);
+		void push_front(T item);
+		
+		T front();
+
+		T back();
+
+
+		~list()
+		{
+
+		}
 
 	};
 }
