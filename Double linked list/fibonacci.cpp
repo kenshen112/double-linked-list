554ba99846b487601394985cc4541e47a72ec579
jessica rose smith <jessi@LinuxLab05.byui.edu>
2019-02-21 08:16:06 -0700

    Finished writing out logic for fibonacci sequence

--- Double linked list/fibonacci.cpp
+++ Double linked list/fibonacci.cpp
@@ -10,7 +10,7 @@
 
 #include <iostream>
 #include "fibonacci.h"   // for fibonacci() prototype
-#include "list.h"        // for LIST
+//#include "list.h"        // for LIST
 using namespace std;
 
 
@@ -27,12 +27,47 @@ void fibonacci()
    cin  >> number;
 
    // your code to display the first <number> Fibonacci numbers
+   int fib1 = 1;
+   int fib2 = 1;
+   int fib3 = 0;
+   
+   cout << fib1 << ", " << fib2 << ", ";
+   int counter = 0;
 
+   while(counter < number)
+   {
+      fib3 = fib1 + fib2;
+      cout <<  fib3 << ", ";
+      
+      fib1 = fib2;
+      fib2 = fib3;
+      
+      counter++;
+   }
+
+   cout << endl;
+   
    // prompt for a single large Fibonacci
    cout << "Which Fibonacci number would you like to display? ";
    cin  >> number;
+   int fib1 = 1;
+   int fib2 = 1;
+   int fib3 = 0;
 
+   cout << fib1 << ", " << fib2 << ", ";
+   
    // your code to display the <number>th Fibonacci number
+   while(fib3 <= number)
+   {
+      fib3 = fib1 + fib2;
+      cout << fib3 << ", ";
+
+      fib1 = fib2;
+      fib2 = fib3;
+
+   }
+   
+   cout << endl;
 }
 
 
